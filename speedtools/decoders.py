37eae81f782812392676adc28f79dd43bc068ca5
Rafał Kuźnia <rafal.kuznia@protonmail.com>
2025-03-21 20:53:21 +0100

    fixup! fixup! decoders

--- speedtools/decoders.py
+++ speedtools/decoders.py
@@ -1,5 +1,5 @@
 #
-# Copyright (c) 2023 Rafał Kuźnia <rafal.kuznia@protonmail.com>
+# Copyright (c) 2025 Rafał Kuźnia <rafal.kuznia@protonmail.com>
 #
 # SPDX-License-Identifier: GPL-3.0-or-later
 #
